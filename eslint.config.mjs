9300b1f27c9c649b095d10a457532d295966086b
hslankinen <lankinenhs@gmail.com>
2025-08-29 20:40:17 +0300

    8th commit for fixing linter errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,8 @@
-import globals from 'globals'
-import js from '@eslint/js'
-import stylisticJs from '@stylistic/eslint-plugin-js'
+import globals from "globals";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
 
 export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      sourceType: 'commonjs',
-      globals: { ...globals.node },
-      ecmaVersion: 'latest',
-    },
-    plugins: {
-      '@stylistic/js': stylisticJs,
-    },
-    rules: {
-      '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
-      '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'never'],
-    },
-  },
-]
+  {languageOptions: { globals: globals.browser }},
+  pluginReactConfig,
+];
