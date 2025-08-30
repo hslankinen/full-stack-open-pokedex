57e16aa8528e4965fbb220d1ca11c227a772d25e
hslankinen <lankinenhs@gmail.com>
2025-08-30 17:57:53 +0300

    13th commit: one e2e test case added

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,9 @@
+const { test, describe, expect, beforeEach } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('butterfree')).toBeVisible()
+  })
+})
