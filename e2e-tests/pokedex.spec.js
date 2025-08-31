1f86bf36dfe34a37187b125cb891caecc13afba6
hslankinen <lankinenhs@gmail.com>
2025-08-31 13:20:09 +0300

    15th commit: Playwright CI set-up for e2e testing with two test cases

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,12 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('butterfree')).toBeVisible()
   })
+
+  test('navigation from front page to other pages works', async ({ page }) => {
+    await page.goto('')  
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+  })
+
 })
