2d300f79ca6faab400222c203b1f1a9140fe6629
hslankinen <lankinenhs@gmail.com>
2025-09-03 16:00:28 +0300

    34th commit: tag action job fixed

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send(' version 0.0.1') // change this string to ensure a new version deployed
+  res.send(' version 0.0.2') // change this string to ensure a new version deployed
 })
