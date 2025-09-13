425851235eb599b9a1a2e4a4b57e2f6f7b4e2f43
hslankinen <lankinenhs@gmail.com>
2025-09-13 15:55:57 +0300

    63rd commit: exercise 11.18 (with Discord WebHook), release tagging success notification

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send(' version 0.0.41') // change this string to ensure a new version deployed
+  res.send(' version 0.0.42') // change this string to ensure a new version deployed
 })
